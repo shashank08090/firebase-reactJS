0039087dff18e030f4a7edec31ce779c67eaf87c
shash08090 <shashank08090@gmail.com>
2020-12-30 16:43:12 +0530

    firebase react app

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,12 @@ import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
   // Your firebase credentials
+  apiKey: "AIzaSyApgi-3oJcufy_tejx0Q5KoqszhYL_pChM",
+    authDomain: "cart-app-247c4.firebaseapp.com",
+    projectId: "cart-app-247c4",
+    storageBucket: "cart-app-247c4.appspot.com",
+    messagingSenderId: "479347284560",
+    appId: "1:479347284560:web:643c2ca3b7876571514f55"
 });
 
 var db = firebaseApp.firestore();
